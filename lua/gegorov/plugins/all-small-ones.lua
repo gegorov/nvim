5bd4bcbe4707d0033b39a52bb89157236af31d14
gegorov <gegorov@yahoo.com>
2024-03-24 22:00:14 +0100

    swithc to harpoon2

--- lua/gegorov/plugins/all-small-ones.lua
+++ lua/gegorov/plugins/all-small-ones.lua
@@ -3,20 +3,6 @@ return {
     -- don't forget to add this one if you don't have it yet!
     "nvim-lua/plenary.nvim",
 
-    -- Harpoon plugin for file quick navigation
-    {
-        "ThePrimeagen/harpoon",
-        config = function()
-            local mark = require("harpoon.mark")
-            local ui = require("harpoon.ui")
-
-            vim.keymap.set("n", "<leader>a", mark.add_file,  { desc = 'Mark file to add to harpoon' } )
-            vim.keymap.set("n", "<leader>h", ui.toggle_quick_menu, { desc = 'Toggle Harpoon quick menu' })
-
-            -- vim.keymap.set("n", "<M-[>", function() ui.nav_prev() end)
-            -- vim.keymap.set("n", "<M-]>", function() ui.nav_next() end)
-        end,
-    },
     -- undo tree
     {
         "mbbill/undotree",
