6bd7d499826425a3c6a48e1d2785076a403d0f17
gegorov <gegorov@yahoo.com>
2024-03-28 00:15:23 +0100

    update telescope config

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,42 +4,29 @@ return {
         version = "0.1.6",
         dependencies = {
             "nvim-lua/plenary.nvim",
+            "nvim-telescope/telescope-ui-select.nvim",
+            {
+                "nvim-telescope/telescope-fzf-native.nvim",
+                build = "make",
+                cond = function()
+                    return vim.fn.executable 'make' == 1
+                end,
+            },
         },
         config = function()
-            require("telescope").setup({})
+            local actions = require("telescope.actions")
+            local telescope = require("telescope")
 
-            local builtin = require("telescope.builtin")
-
-            vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files" })
-            vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live grep everything" })
-            vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Find buffers" })
-            vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Find in help" })
-            vim.keymap.set("n", "<leader>fk", builtin.keymaps, { desc = "Find in keymaps" })
-            vim.keymap.set("n", "<C-p>", builtin.git_files, { desc = "Search in git files" })
-            vim.keymap.set("n", "<leader>s", function()
-                builtin.grep_string({ search = vim.fn.input("Grep > ") })
-            end, { desc = "Grep: search phrase in all files" })
-            -- Shortcut for searching your Neovim configuration files
-            vim.keymap.set("n", "<leader>fn", function()
-                builtin.find_files({ cwd = vim.fn.stdpath("config") })
-            end, { desc = "[F]ind [N]eovim files" })
-
-            vim.keymap.set("n", "<leader>fw", function()
-                local word = vim.fn.expand("<cword>")
-                builtin.grep_string({ search = word })
-            end, { desc = "Search for exact WORD under cursor" })
-
-            vim.keymap.set("n", "<leader>fW", function()
-                local word = vim.fn.expand("<cWORD>")
-                builtin.grep_string({ search = word })
-            end, { desc = "Search for exact PHRASE under cursor" })
-        end,
-    },
-    {
-        "nvim-telescope/telescope-ui-select.nvim",
-        config = function()
-            -- This is your opts table
-            require("telescope").setup({
+            telescope.setup({
+                defaults = {
+                    mappings = {
+                        i = {
+                            ["<C-k>"] = actions.move_selection_previous,
+                            ["<C-j>"] = actions.move_selection_next,
+                            ["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
+                        },
+                    },
+                },
                 extensions = {
                     ["ui-select"] = {
                         require("telescope.themes").get_dropdown({
@@ -60,11 +47,44 @@ return {
                         --   codeactions = false,
                         -- }
                     },
+                    ["fzf"] = {
+                        fuzzy = true, -- false will only do exact matching
+                        override_generic_sorter = true, -- override the generic sorter
+                        override_file_sorter = true, -- override the file sorter
+                        case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+                    },
                 },
             })
-            -- To get ui-select loaded and working with telescope, you need to call
-            -- load_extension, somewhere after setup function:
-            require("telescope").load_extension("ui-select")
+
+            telescope.load_extension("ui-select")
+            telescope.load_extension("fzf")
+
+            local builtin = require("telescope.builtin")
+
+            vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files" })
+            vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live grep everything" })
+            vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Find buffers" })
+            vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Find in help" })
+            vim.keymap.set("n", "<leader>fr", builtin.resume, { desc = "Resume previous Find" })
+            vim.keymap.set("n", "<leader>fk", builtin.keymaps, { desc = "Find in keymaps" })
+            vim.keymap.set("n", "<leader>fF", builtin.git_files, { desc = "Search in git files" })
+            vim.keymap.set("n", "<leader>s", function()
+                builtin.grep_string({ search = vim.fn.input("Grep > ") })
+            end, { desc = "Grep: search phrase in all files" })
+            -- Shortcut for searching your Neovim configuration files
+            vim.keymap.set("n", "<leader>fn", function()
+                builtin.find_files({ cwd = vim.fn.stdpath("config") })
+            end, { desc = "[F]ind [N]eovim files" })
+
+            vim.keymap.set("n", "<leader>fw", function()
+                local word = vim.fn.expand("<cword>")
+                builtin.grep_string({ search = word })
+            end, { desc = "Search for exact WORD under cursor" })
+
+            vim.keymap.set("n", "<leader>fW", function()
+                local word = vim.fn.expand("<cWORD>")
+                builtin.grep_string({ search = word })
+            end, { desc = "Search for exact PHRASE under cursor" })
         end,
     },
 }
